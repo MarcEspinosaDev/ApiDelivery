a88c5c1b3ac9c16bb0d7b159a31d871753d3efcf
BuildTools <unconfigured@null.spigotmc.org>
2022-10-05 21:00:23 +0200

    Sesion 11 - CRUD // NO va "Update Delete"

--- src/Controller/Api/CategoriaController.php
+++ src/Controller/Api/CategoriaController.php
@@ -2,21 +2,51 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Categoria;
+use App\Form\CategoriaType;
 use App\Repository\CategoriaRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Rest\Route("/categoria")
  */
 class CategoriaController extends AbstractFOSRestController
 {
+    //CRUD
+    // Create, Update, Delete, Read
+
     private $categoriaRepository;
 
     public function __construct(CategoriaRepository $repo){
         $this->categoriaRepository = $repo;
     }
 
+    //CREATE
+    /**
+     * @Rest\Post(path="/")
+     * @Rest\View (serializerGroups={"post_categorias"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function postCategorias(Request $request){
+        // Formularios
+        // 1- crear objeto vacio
+        $category = new Categoria();
+        //2- inicializamos el form
+        $form = $this->createForm(CategoriaType::class, $category);
+        // 3- El form maneja el request
+        $form->handleRequest($request);
+        // 4- Comprobar que no hay error
+        if(!$form->isSubmitted() || !$form->isValid()){
+            return $form;
+        }
+        //5- bien tot y a la bd
+        $this->categoriaRepository->add($category, true);
+        return $category;
+    }
+    // READ all
     /**
      * @Rest\Get(path="/")
      * @Rest\View(serializerGroups={"get_categorias"}, serializerEnableMaxDepthChecks=true)
@@ -24,4 +54,55 @@ class CategoriaController extends AbstractFOSRestController
     public function getCategorias(){
         return $this->categoriaRepository->findAll();
     }
+
+    //READ one by id
+    /**
+     * @Rest\Get(path="/{id}")
+     * @Rest\View(serializerGroups={"get_categoria"}, serializerEnableMaxDepthChecks= true)
+     */
+    public function getCategoria(Request $request){
+        $category = $this->categoriaRepository->find($request->get('id'));
+        if (!$category){
+            return new JsonResponse('Cannot find that category', Response::HTTP_NOT_FOUND);
+        }
+        return $category;
+    }
+
+    // !!!!!!!!! no va el UPDATE ni el DELETE !!!!!!!!!!!
+
+    //UPDATE (Patch)
+    /**
+     * @Rest\Patch(path="/{id}")
+     * @Rest\View(serializerGroups={"update_categorias"}, serializerEnableMaxDepthChecks= true)
+     */
+
+    public function updateCategoria(Request $request){
+        $idCategory = $request->get('id');
+        if(!$this->categoriaRepository->find($idCategory)){
+            return new JsonResponse('Not found', Response::HTTP_NOT_FOUND);
+        }
+        $form = $this->createForm(CategoriaType::class,$idCategory,['method'=>$request->getMethod()]);
+        $form->handleRequest($request);
+        //comprobar la validez del form
+        if(!$form->isSubmitted() || !$form->isValid()){
+            return new JsonResponse('Bad Data', 400);
+        }
+        $this->categoriaRepository->add($idCategory, true);
+        return $idCategory;
+    }
+
+    //DELETE
+    /**
+     * @Rest\Delete(path="/")
+     */
+    public function deleteCategorias(Request $request){
+        $categoriaId = $request->get('id');
+        $categoria = $this->categoriaRepository->find($categoriaId);
+        if (!$categoria){
+            return new JsonResponse('Not Found', 400);
+        }
+        $this->categoriaRepository->remove($categoria, true);
+        return new JsonResponse('Deleted category',200);
+    }
 }
+
